b57470aa8bed290e26ea7a6fe479f12488634202
Tiny-Lealta <kevkev-lp@web.de>
2026-06-04 11:49:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ===== COUNTDOWN =====
 
-const targetDate = new Date("2026-06-01T00:00:00").getTime();
+const targetDate = new Date("2026-06-30T00:00:00").getTime();
 
 function updateCountdown() {
   const now = new Date().getTime();
